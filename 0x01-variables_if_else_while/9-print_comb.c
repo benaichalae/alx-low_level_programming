409b861ba4b6f99322cff59f4dafef0e62dbb7d6
benaichalae <benaichalae@gmail.com>
2023-06-16 14:20:12 -0700

    combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,10 +8,16 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n < 10; n++)
+	for (n = 48; n <= 57; n++)
 	{
-	printf("%d, ", n);
+	putchar(n);
+	if (n == 57)
+	{
+	continue;
 	}
-	printf("\n");
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
 	return (0);
 }
