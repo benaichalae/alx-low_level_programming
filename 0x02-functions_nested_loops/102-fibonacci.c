5ed0f97169c6642323a3e29c9a75822413ddf172
benaichalae <benaichalae@gmail.com>
2023-06-20 15:09:28 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - print 50 Fibonacci numbers
+ * Return: always 0
+ */
+int main(void)
+{
+	int counter;
+	unsigned long i, j, k;
+
+	counter = 0;
+	while (counter < 50)
+	{
+		k = i + j;
+		printf("%lu", k);
+		i = j;
+		j = k;
+		if (counter == 49)
+			printf("\n");
+		else
+			printf(", ");
+		counter++;
+	}
+	return (0);
+}
