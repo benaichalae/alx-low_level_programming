78b14a6759bc73ec1ab4c55555d7d87c65d9e1fb
benaichalae <benaichalae@gmail.com>
2023-06-20 14:51:20 -0700

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *
+ */
+int main(void)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (i < 1024)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			j += i;
+		i++;
+	}
+	printf("%d\n", j);
+	return (0);
+}
