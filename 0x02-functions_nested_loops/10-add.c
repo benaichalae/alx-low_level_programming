045b1aa08dbbc985a14c31f9997bf307e70aff1c
benaichalae <benaichalae@gmail.com>
2023-06-20 08:53:27 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *add - add two integers
+ *@a: int to be added to b
+ *@b: int to be added to a
+ *Return: sun of a and b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
