ca66c46561e4771a138f2463fd81708139d86914
benaichalae <benaichalae@gmail.com>
2023-07-05 06:27:09 -0700

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,7 +2,7 @@
 /**
  * factorial - function that returns the factorial of a given number
  * @n: the int that will be calculated
- * Return : -1 if n is lower than 0, 1 if n equal to 0 and the factorial of n
+ * Return: -1 if n is lower than 0, 1 if n equal to 0 and the factorial of n
  */
 int factorial(int n)
 {
