1881d5934891d2829da4496533a8072ddd1b737e
benaichalae <benaichalae@gmail.com>
2023-07-07 07:28:01 -0700

    Heisenberg

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 /**
- *
+ * main -program that prints its name
+ * @argc: argument count
+ * @argv: arguments
+ * Return: 0
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
