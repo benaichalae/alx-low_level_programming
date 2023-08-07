6eb85783fbeacdc287336651672678c2dec7346e
benaichalae <benaichalae@gmail.com>
2023-08-07 06:45:10 -0700

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,14 +8,14 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, bytes_write, len = 0;
+	int fd, bytes_write, x = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
+		for (len = 0; text_content[x];)
 			len++;
 	}
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
