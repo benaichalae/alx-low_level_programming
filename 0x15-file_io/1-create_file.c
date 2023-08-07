7d7bea55facf4315ac14fff259a3f4fd13bc3047
benaichalae <benaichalae@gmail.com>
2023-08-07 06:36:33 -0700

    creat

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: pointer to a file
+ * @text_content: terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, bytes_write, len;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		len = strlen(text_content);
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	bytes_write = write(fd, text_content, len);
+
+	if (fd == -1 || bytes_write == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
