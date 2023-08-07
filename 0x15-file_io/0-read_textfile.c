0eccafb682a764601cea132eb9fd00169c662f62
benaichalae <benaichalae@gmail.com>
2023-08-07 06:23:47 -0700

    read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 
-	buf = malloc(sizeof(cahr) * letters);
+	buf = malloc(sizeof(char) * letters);
 	bytes_read = read(fd, buf, letters);
 	bytes_write = write(STDOUT_FILENO, buf, bytes_read);
 
