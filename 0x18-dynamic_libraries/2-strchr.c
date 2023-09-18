3b3033e122b0e193b4df1d4f044165e12a54d509
Alae ben aicha <75539159+benaichalae@users.noreply.github.com>
2023-09-18 14:49:16 -0700

    Add files via upload

---
+++ 0x18-dynamic_libraries/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - function to search char c in a string s
+ * @s: the string that we will search on
+ * @c: the character that we are looking for
+ * Return: the pointer of the character c on string s or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
+}
+
---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute of a number
+ * @n: the int to check
+ * Return: the absolute value
+ */
+int _abs(int n)
+{
+	if (n <= 0)
+	{
+		return (-n);
+	}
+	return (n);
+}
